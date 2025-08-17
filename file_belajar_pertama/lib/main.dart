9b5156a0d1c1ac153bda695ca181d669d722e733
yasirkhoirul <yasirkhoirul9@gmail.com>
2025-08-18 01:20:26 +0700

    memisahkan class gradient container

--- file_belajar_pertama/lib/main.dart
+++ file_belajar_pertama/lib/main.dart
@@ -1,32 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:file_belajar_pertama/gradient_container.dart';
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer()
+        body: const GradientContainer()
       ),
     ),
   );
 }
 
-class GradientContainer extends StatelessWidget{
-  @override// harus sama parameternya ini maksud nya menimpa
-  Widget build(BuildContext context) {
-    return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color.fromARGB(255, 234, 208, 235), Colors.deepPurple],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "belajar tentang widget pada flutter",
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-          ),
-        );
-  }
-}
+
